c88814dfa1dc55caecb2b1bbd01189b5669a7ba2
Fitter22 <fitter22@hotmail.com>
2018-03-14 16:32:58 +0100

    Code refactoring

--- scripts/filter_scene_and_select_grasp.py
+++ scripts/filter_scene_and_select_grasp.py
@@ -3,6 +3,10 @@ import subprocess
 import rospy
 import pcl
 import actionlib
+import numpy as np
+from scipy.linalg import lstsq
+from std_msgs.msg import Header, Int64
+from geometry_msgs.msg import Point
 from plane_segm import filterCloud
 from sensor_msgs.msg import PointCloud2
 from sensor_msgs import point_cloud2
@@ -30,93 +34,64 @@ class PointHeadClient(object):
         self.client.wait_for_result()
 
 
-i = 0
-rawCloud = []  # global variable to store the point cloud
+class GpdGrasps(object):
+    rawCloud = []
+    filtered_cloud = []
+    i = 0
+
+    def __init__(self):
+        # Subscribe to the ROS topic that contains the grasps.
+        rospy.Subscriber("/head_camera/depth_downsample/points", PointCloud2, self.cloud_callback)
+
+    def cloud_callback(self, msg):
+        if self.i < 8:
+            self.i += 1
+            for p in point_cloud2.read_points(msg, skip_nans=True):
+                self.rawCloud.append([p[0], p[1], p[2]])
+
+    def filter_cloud(self):
+        # Wait for point cloud to arrive.
+        while self.i < 8:
+            rospy.sleep(0.01)
+
+        #self.filtered_cloud = pcl.PointCloud()
+        self.filtered_cloud = filterCloud(self.rawCloud)
+
+        # Publish cloud with extracted obstacles to create octomap
+        subprocess.Popen(
+            ['rosrun', 'pcl_ros', 'pcd_to_pointcloud', 'obstacles.pcd', '_frame_id:=head_camera_rgb_optical_frame'])
+
+    def extract_indices(self):
+        # Extract the nonplanar indices. Uses a least squares fit AX = b. Plane equation: z = ax + by + c.
+
+        np_cloud = self.filtered_cloud.to_array()
+        X = np_cloud
+        A = np.c_[X[:, 0], X[:, 1], np.ones(X.shape[0])]
+        C, _, _, _ = lstsq(A, X[:, 2])
+        a, b, c, d = C[0], C[1], -1., C[2]  # coefficients of the form: a*x + b*y + c*z + d = 0.
+        dist = ((a * X[:, 0] + b * X[:, 1] + d) - X[:, 2]) ** 2
+        # err = dist.sum()
+        idx = np.where(dist > 0.001)
+
+        return np_cloud, idx
+
+    def publish_cloud_indexed(self):
+        np_cloud, idx = self.extract_indices()
+
+        pub = rospy.Publisher('cloud_indexed', CloudIndexed, queue_size=1, latch=True)
+        msg = CloudIndexed()
+        header = Header()
+        header.frame_id = "head_camera_rgb_optical_frame"
+        header.stamp = rospy.Time.now()
+        msg.cloud_sources.cloud = point_cloud2.create_cloud_xyz32(header, np_cloud.tolist())
+        msg.cloud_sources.view_points.append(Point(0, 0, 0))
+
+        for i in xrange(np_cloud.shape[0]):
+            msg.cloud_sources.camera_source.append(Int64(0))
+        for i in idx[0]:
+            msg.indices.append(Int64(i))
+        pub.publish(msg)
+        rospy.sleep(3.14)
+        print 'Published cloud with', len(msg.indices), 'indices'
 
 
-def cloudCallback(msg):
-    global rawCloud
-    global i
-    # if len(rawCloud) == 0:
-    if i < 8:
-        i += 1
-        for p in point_cloud2.read_points(msg, skip_nans=True):
-            rawCloud.append([p[0], p[1], p[2]])
-
-
-rospy.init_node('select_grasp')
-
-head = PointHeadClient()
-print "Moving head"
-head.look_at(2, 0.0, 0.0, "base_link")
-
-# Subscribe to the ROS topic that contains the grasps.
-cloud_sub = rospy.Subscriber("/head_camera/depth_downsample/points", PointCloud2, cloudCallback)
-
-# Wait for point cloud to arrive.
-while i < 8:
-    rospy.sleep(0.01)
-
-cloud = pcl.PointCloud()
-cloud = filterCloud(rawCloud)
-
-p = subprocess.Popen(
-    ['rosrun', 'pcl_ros', 'pcd_to_pointcloud', 'obstacles.pcd', '_frame_id:=head_camera_rgb_optical_frame'])
-
-# Extract the nonplanar indices. Uses a least squares fit AX = b. Plane equation: z = ax + by + c.
-import numpy as np
-from scipy.linalg import lstsq
-
-cloud2 = cloud.to_array()
-X = cloud2
-A = np.c_[X[:, 0], X[:, 1], np.ones(X.shape[0])]
-C, _, _, _ = lstsq(A, X[:, 2])
-a, b, c, d = C[0], C[1], -1., C[2]  # coefficients of the form: a*x + b*y + c*z + d = 0.
-dist = ((a * X[:, 0] + b * X[:, 1] + d) - X[:, 2]) ** 2
-err = dist.sum()
-idx = np.where(dist > 0.001)
-
-from std_msgs.msg import Header, Int64
-from geometry_msgs.msg import Point
-
-pub = rospy.Publisher('cloud_indexed', CloudIndexed, queue_size=1, latch=True)
-msg = CloudIndexed()
-header = Header()
-header.frame_id = "head_camera_rgb_optical_frame"
-header.stamp = rospy.Time.now()
-msg.cloud_sources.cloud = point_cloud2.create_cloud_xyz32(header, cloud2.tolist())
-msg.cloud_sources.view_points.append(Point(0, 0, 0))
-
-for i in xrange(cloud2.shape[0]):
-    msg.cloud_sources.camera_source.append(Int64(0))
-for i in idx[0]:
-    msg.indices.append(Int64(i))
-rospy.sleep(3.14)
-pub.publish(msg)
-print 'Published cloud with', len(msg.indices), 'indices'
-print 'Searching for grasps...'
-
-# Select a grasp for the robot to execute.
-from gpd.msg import GraspConfigList
-
-grasps = []  # global variable to store grasps
-
-
-def callback(msg):
-    global grasps
-    grasps = msg.grasps
-
-
-# Subscribe to the ROS topic that contains the grasps.
-grasps_sub = rospy.Subscriber('/detect_grasps/clustered_grasps', GraspConfigList, callback)
-
-# Wait for grasps to arrive.
-rate = rospy.Rate(1)
-
-while not rospy.is_shutdown():
-    if len(grasps) > 0:
-        rospy.loginfo('Received %d grasps.', len(grasps))
-        break
-
-grasp = grasps[0]  # grasps are sorted in descending order by score
-print 'Selected grasp with score:', grasp.score
--- scripts/pick_and_place.py
+++ scripts/pick_and_place.py
@@ -4,120 +4,147 @@ from pprint import pprint
 from pyquaternion import Quaternion
 from gpd.msg import GraspConfigList
 from moveit_python import *
-from moveit_msgs.msg import Grasp, PlaceLocation, MoveItErrorCodes
-from geometry_msgs.msg import PoseStamped
-from tf.transformations import quaternion_from_euler
+from moveit_msgs.msg import Grasp
+from geometry_msgs.msg import PoseStamped, Vector3
 from trajectory_msgs.msg import JointTrajectoryPoint
-from geometry_msgs.msg import Pose, Point, Vector3
 from visualization_msgs.msg import Marker
 from std_msgs.msg import Header, ColorRGBA
+from filter_scene_and_select_grasp import PointHeadClient, GpdGrasps
 
-grasps = []
-formatted_grasps = []
 
 def print_color(text):
     print str('\033[33m' + '\033[1m' + text + '\033[0m')
 
+class GpdPickPlace(object):
+    grasps = []
+    mark_pose = False
 
-def grasp_callback(msg):
-    global grasps
-    grasps = msg.grasps
-    print_color("Received new grasps")
-
-
-def show_grasp_pose(publisher, grasp_pose):
-    print_color("Arrow orientation:")
-    pprint(grasp_pose.orientation)
-    marker = Marker(
-        type=Marker.ARROW,
-        id=0,
-        lifetime=rospy.Duration(30),
-        pose=grasp_pose,
-        scale=Vector3(0.03, 0.03, 0.03),
-        header=Header(frame_id='head_camera_rgb_optical_frame'),
-        color=ColorRGBA(0.0, 1.0, 0.0, 0.8))
-    publisher.publish(marker)
+    def __init__(self, mark_pose=False):
+        rospy.Subscriber("/detect_grasps/clustered_grasps", GraspConfigList, self.grasp_callback)
+        if mark_pose:
+            self.mark_pose = True
+            self.marker_publisher = rospy.Publisher('visualization_marker', Marker, queue_size=5)
+        self.p = PickPlaceInterface("arm", "gripper", verbose=True)
 
 
-rospy.init_node("gpd_pick_and_place")
-print_color("Waiting for grasps to arrive")
-rospy.Subscriber("/detect_grasps/clustered_grasps", GraspConfigList, grasp_callback)
-marker_publisher = rospy.Publisher('visualization_marker', Marker, queue_size=5)
+    def grasp_callback(self, msg):
+        self.grasps = msg.grasps
+        print_color("Received new grasps")
 
-# wait for grasps
-# while len(grasps) == 0:
-#	rospy.sleep(0.01)
+    def show_grasp_pose(self, publisher, grasp_pose):
+        # print_color("Marker orientation:")
+        # pprint(grasp_pose.orientation)
+        marker = Marker(
+            type=Marker.ARROW,
+            id=0,
+            lifetime=rospy.Duration(30),
+            pose=grasp_pose,
+            scale=Vector3(0.03, 0.02, 0.02),
+            header=Header(frame_id='head_camera_rgb_optical_frame'),
+            color=ColorRGBA(0.0, 1.0, 0.0, 0.8))
+        publisher.publish(marker)
 
-rospy.sleep(1)
+    def get_gpd_grasps(self):
+        print_color("Waiting for grasps to arrive")
+        while len(self.grasps) == 0:
+            rospy.sleep(0.01)
+        return self.grasps
 
-p = PickPlaceInterface("arm", "gripper", verbose=True)
+    def generate_grasp_msgs(self, grasps):
+        formatted_grasps = []
+        for i in range(0, 5):
+            g = Grasp()
+            g.id = "dupa"
+            gp = PoseStamped()
+            gp.header.frame_id = "head_camera_rgb_optical_frame"
+            gp.pose.position.x = grasps[i].surface.x
+            gp.pose.position.y = grasps[i].surface.y
+            gp.pose.position.z = grasps[i].surface.z
 
-for i in range(0, 5):
-    g = Grasp()
-    g.id = "dupa"
-    gp = PoseStamped()
-    gp.header.frame_id = "head_camera_rgb_optical_frame"
-    gp.pose.position.x = grasps[i].surface.x
-    gp.pose.position.y = grasps[i].surface.y
-    gp.pose.position.z = grasps[i].surface.z
+            quat = self.trans_matrix_to_quaternion(grasps, i)
 
-    r = np.array([[grasps[i].approach.x, grasps[i].approach.y, grasps[i].approach.z],
-                  [grasps[i].binormal.x, grasps[i].binormal.y, grasps[i].binormal.z],
-                  [grasps[i].axis.x, grasps[i].axis.y, grasps[i].axis.z]])
+            gp.pose.orientation.x = float(quat.elements[1])
+            gp.pose.orientation.y = float(quat.elements[2])
+            gp.pose.orientation.z = float(quat.elements[3])
+            gp.pose.orientation.w = - float(quat.elements[0])  # ??
 
-    quat = Quaternion(matrix=r)
+            g.grasp_pose = gp
 
-    gp.pose.orientation.x = float(quat.elements[1])
-    gp.pose.orientation.y = float(quat.elements[2])
-    gp.pose.orientation.z = float(quat.elements[3])
-    gp.pose.orientation.w = - float(quat.elements[0])
+            g.pre_grasp_approach.direction.header.frame_id = "gripper_link"
+            g.pre_grasp_approach.direction.vector.x = 1.0
+            g.pre_grasp_approach.direction.vector.y = 0.0
+            g.pre_grasp_approach.direction.vector.z = 0.0
+            g.pre_grasp_approach.min_distance = 0.07
+            g.pre_grasp_approach.desired_distance = 0.20
 
-    g.grasp_pose = gp
+            g.pre_grasp_posture.joint_names = ["l_gripper_finger_joint", "r_gripper_finger_joint"]
+            pos = JointTrajectoryPoint()
+            pos.positions.append(0.055)
+            pos.positions.append(0.055)
+            g.pre_grasp_posture.points.append(pos)
 
-    g.pre_grasp_approach.direction.header.frame_id = "gripper_link"
-    g.pre_grasp_approach.direction.vector.x = 1.0
-    g.pre_grasp_approach.direction.vector.y = 0.0
-    g.pre_grasp_approach.direction.vector.z = 0.0
-    g.pre_grasp_approach.min_distance = 0.07
-    g.pre_grasp_approach.desired_distance = 0.20
+            g.grasp_posture.joint_names = ["l_gripper_finger_joint", "r_gripper_finger_joint"]
+            pos = JointTrajectoryPoint()
+            pos.positions.append(0.0)
+            pos.positions.append(0.0)
+            pos.effort.append(50.0)
+            pos.effort.append(50.0)
+            g.grasp_posture.points.append(pos)
 
-    g.pre_grasp_posture.joint_names = ["l_gripper_finger_joint", "r_gripper_finger_joint"]
-    pos = JointTrajectoryPoint()
-    pos.positions.append(0.055)
-    pos.positions.append(0.055)
-    g.pre_grasp_posture.points.append(pos)
+            g.allowed_touch_objects = ["<octomap>"]
+            g.max_contact_force = 0.0
+            g.grasp_quality = grasps[0].score.data
 
-    g.grasp_posture.joint_names = ["l_gripper_finger_joint", "r_gripper_finger_joint"]
-    pos = JointTrajectoryPoint()
-    pos.positions.append(0.0)
-    pos.positions.append(0.0)
-    pos.effort.append(50.0)
-    pos.effort.append(50.0)
-    g.grasp_posture.points.append(pos)
+            formatted_grasps.append(g)
+        return formatted_grasps
 
-    g.allowed_touch_objects = ["<octomap>"]
-    g.max_contact_force = 0.0
-    g.grasp_quality = grasps[0].score.data
+    @staticmethod
+    def trans_matrix_to_quaternion(grasps, i):
+        r = np.array([[grasps[i].approach.x, grasps[i].approach.y, grasps[i].approach.z],
+                      [grasps[i].binormal.x, grasps[i].binormal.y, grasps[i].binormal.z],
+                      [grasps[i].axis.x, grasps[i].axis.y, grasps[i].axis.z]])
+        return Quaternion(matrix=r)
 
-    formatted_grasps.append(g)
+    def pick(self, grasps_list, verbose=False):
+        print_color("Pick sequence started")
 
-rospy.sleep(1)
-print_color("Pick sequence started")
+        for single_grasp in grasps_list:
+            if self.mark_pose:
+                self.show_grasp_pose(self.marker_publisher, single_grasp.grasp_pose.pose)
 
-for single_grasp in formatted_grasps:
-    show_grasp_pose(marker_publisher, single_grasp.grasp_pose.pose)
+            if verbose:
+                print_color("Executing grasp: ")
+                pprint(single_grasp.grasp_pose.pose)
 
-    print_color("Executing grasp: ")
-    pprint(single_grasp.grasp_pose.pose)
+            rospy.sleep(1)
+            pick_result = self.p.pickup("obj", [single_grasp, ], planning_time=9001, support_name="<octomap>",
+                                        allow_gripper_support_collision=True)
 
-    rospy.sleep(1)
-    pick_result = p.pickup("obj", [single_grasp, ], planning_time=9001, support_name="<octomap>",
-                           allow_gripper_support_collision=True)
+            if pick_result.error_code.val != -1:
+                print_color("Done")
+                break
+            print_color("Planer failed")
 
-    if pick_result.error_code.val != -1:
-        print_color("Done")
-        break
-    print_color("Planer failed")
+
+if __name__ == "__main__":
+    rospy.init_node("gpd_pick_and_place")
+
+    # Tilt the head down to see the table
+    head = PointHeadClient()
+    print_color("Moving head")
+    head.look_at(2.0, 0.0, 0.0, "base_link")
+
+    # Get the pointcloud from camera, filter it, extract indices and publish it to gpd CNN
+    gpd_prep = GpdGrasps()
+    gpd_prep.filter_cloud()
+    gpd_prep.publish_cloud_indexed()
+
+    pnp = GpdPickPlace(mark_pose=False)
+    selected_grasps = pnp.get_gpd_grasps()
+    formatted_grasps = pnp.generate_grasp_msgs(selected_grasps)
+    pnp.pick(formatted_grasps, verbose=True)
+
+    # pnp.pick(pnp.generate_grasp_msgs(pnp.get_gpd_grasps()), verbose=True)
 
 # rospy.spin()
 
