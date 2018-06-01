55c882df80c683dbc40645636df2cd1cf2273bcf
Fitter22 <fitter22@hotmail.com>
2018-06-01 13:58:58 +0200

    Clean gripper hack a little bit

--- scripts/gripper_hack.py
+++ scripts/gripper_hack.py
@@ -7,10 +7,8 @@ from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryA
 from play_motion_msgs.msg import PlayMotionAction, PlayMotionGoal
 from actionlib import SimpleActionClient, SimpleActionServer
 
-class HackyGripperController:
-    last_arm_goal = ""
-    is_it_second_callback = False
 
+class HackyGripperController:
     def __init__(self):
         rospy.init_node('gripper_controller')
 
@@ -25,28 +23,30 @@ class HackyGripperController:
 
         pevent("Hacky Gripper Controller: Initialized")
 
+        self.pmg = PlayMotionGoal()
+
         rospy.spin()
 
     def gripper_callback(self, goal):
         # this can be useful:
         # https://github.com/pal-robotics/pal_gripper/blob/indigo-devel/pal_gripper_controller_configuration/scripts/home_gripper.py
-        pmg = PlayMotionGoal()
 
         # Pick'nPlace API wants to close the gripper, so do it
         if goal.trajectory.points[1].positions[0] == 0.0 and goal.trajectory.points[0].positions[0] > 0.04:
             pevent("Closing the gripper")
-            pmg.motion_name = 'close_gripper'
+            self.pmg.motion_name = 'close_gripper'
 
         # open the gripper
         elif goal.trajectory.points[0].positions[0] != 0.0:
             pevent("Opening the gripper")
-            pmg.motion_name = 'open_gripper'
+            self.pmg.motion_name = 'open_gripper'
 
-        pmg.skip_planning = False
-        self.play_m_as.send_goal_and_wait(pmg)
+        if len(self.pmg.motion_name) > 0:
+            self.pmg.skip_planning = False
+            self.play_m_as.send_goal_and_wait(self.pmg)
 
-        self.server.set_succeeded()
-        pinfo("Hacky Gripper Controller: Done.")
+            self.server.set_succeeded()
+            pinfo("Hacky Gripper Controller: Done.")
 
 
 if __name__ == '__main__':
