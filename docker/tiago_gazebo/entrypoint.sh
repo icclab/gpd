4294579e5afa6c2c1995c965dc4cda7caf461b12
Fitter22 <fitter22@hotmail.com>
2018-07-23 16:54:32 +0200

    Add entrypoint and upgrade Gazebo to 7.4

---
+++ docker/tiago_gazebo/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set +e
+
+source "/catkin_ws/devel/setup.bash"
+
+Xvfb :1 -screen 0 1600x1200x16  &
+export DISPLAY=:1.0
+
+python /catkin_ws/src/gpd/scripts/gripper_hack.py &
+sleep 2
+
+roslaunch /catkin_ws/src/gpd/launch/tiago_playground_headless.launch &
+sleep 20
+
+python /catkin_ws/src/gpd/scripts/downsample_cloud.py
+
+exec "$@"
