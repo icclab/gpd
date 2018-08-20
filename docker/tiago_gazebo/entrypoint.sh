0c0972e8abbc5912cbe69a71f7bd8e6cb83068cc
Fitter22 <fitter22@hotmail.com>
2018-08-20 14:47:29 +0200

    Adjust cloud demo configs

--- docker/tiago_gazebo/entrypoint.sh
+++ docker/tiago_gazebo/entrypoint.sh
@@ -9,7 +9,7 @@ export DISPLAY=:1.0
 python /catkin_ws/src/gpd/scripts/gripper_hack.py &
 sleep 2
 
-roslaunch /catkin_ws/src/gpd/launch/tiago_playground_headless.launch &
+roslaunch /catkin_ws/src/gpd/launch/tiago_playground_no_gpd_headless.launch &
 sleep 20
 
 python /catkin_ws/src/gpd/scripts/downsample_cloud.py
