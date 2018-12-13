52ea0fedb353248450fc6bb6e6f2cb4123722cec
Giovanni Toffetti <toff@zhaw.ch>
2018-12-13 18:07:36 +0100

    made invocation to pointcloud filtering importable

--- scripts/filter_pointcloud_client.py
+++ scripts/filter_pointcloud_client.py
@@ -5,14 +5,15 @@ import rospy
 from std_srvs.srv import Trigger
 
 def call_pointcloud_filter_service():
+    print "Requesting point cloud filtering"
     rospy.wait_for_service('filter_pointcloud')
     try:
         service_proxy = rospy.ServiceProxy('filter_pointcloud', Trigger)
         resp1 = service_proxy()
+        print "Service invoked"
         return resp1
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
 
 if __name__ == "__main__":
-    print "Requesting point cloud filtering"
     call_pointcloud_filter_service()
