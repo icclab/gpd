81bbcbb16609b07e503c5d4cb1c7bc422383cebc
Giovanni Toffetti <toff@zhaw.ch>
2019-02-20 17:03:53 +0100

    convertToVisualGraspMsg was not properly defined

--- include/nodes/grasp_detection_node.h
+++ include/nodes/grasp_detection_node.h
@@ -62,7 +62,6 @@
 
 // this project (headers)
 #include "../gpd/grasp_detector.h"
-#include "../gpd/grasp_plotter.h"
 #include "../gpd/sequential_importance_sampling.h"
 
 #include <gpd/SetParameters.h>
@@ -94,9 +93,13 @@ public:
   ~GraspDetectionNode()
   {
     delete cloud_camera_;
-    delete importance_sampling_;
+
+    if (use_importance_sampling_)
+    {
+      delete importance_sampling_;
+    }
+
     delete grasp_detector_;
-    delete rviz_plotter_;
   }
 
   /**
@@ -167,6 +170,15 @@ private:
 
   gpd::GraspConfig convertToGraspMsg(const Grasp& hand);
 
+  visualization_msgs::MarkerArray convertToVisualGraspMsg(const std::vector<Grasp>& hands, double outer_diameter,
+    double hand_depth, double finger_width, double hand_height, const std::string& frame_id);
+
+  visualization_msgs::Marker createFingerMarker(const Eigen::Vector3d& center, const Eigen::Matrix3d& frame,
+    double length, double width, double height, int id, const std::string& frame_id);
+
+  visualization_msgs::Marker createHandBaseMarker(const Eigen::Vector3d& start, const Eigen::Vector3d& end,
+      const Eigen::Matrix3d& frame, double length, double height, int id, const std::string& frame_id);
+
   Eigen::Matrix3Xd fillMatrixFromFile(const std::string& filename, int num_normals);
 
   Eigen::Vector3d view_point_; ///< (input) view point of the camera onto the point cloud
@@ -186,12 +198,17 @@ private:
   ros::ServiceServer srv_set_params_; ///< ROS service server for setting params
 
   bool use_importance_sampling_; ///< if importance sampling is used
+  bool filter_grasps_; ///< if grasps are filtered on workspace and gripper aperture
+  bool filter_half_antipodal_; ///< if half-antipodal grasps are filtered
+  bool plot_filtered_grasps_; ///< if filtered grasps are plotted
+  bool plot_selected_grasps_; ///< if selected grasps are plotted
+  bool plot_normals_; ///< if normals are plotted
+  bool plot_samples_; ///< if samples/indices are plotted
   bool use_rviz_; ///< if rviz is used for visualization instead of PCL
   std::vector<double> workspace_; ///< workspace limits
 
-  GraspDetector* grasp_detector_; ///< used to run the GPD algorithm
-  SequentialImportanceSampling* importance_sampling_; ///< sequential importance sampling variation of GPD algorithm
-  GraspPlotter* rviz_plotter_; ///< used to plot detected grasps in rviz
+  GraspDetector* grasp_detector_; ///< used to run the grasp pose detection
+  SequentialImportanceSampling* importance_sampling_; ///< sequential importance sampling variation of grasp pose detection
 
   /** constants for input point cloud types */
   static const int POINT_CLOUD_2; ///< sensor_msgs/PointCloud2
